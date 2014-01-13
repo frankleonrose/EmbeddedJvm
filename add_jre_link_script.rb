380da6105901e1faa6ff6af341cd1df27662db62
Frank Leon Rose <frankleonrose@gmail.com>
2014-01-13 10:01:32 -0500

    Experiments with adding JRE script.

--- add_jre_link_script.rb
+++ add_jre_link_script.rb
@@ -1,5 +1,7 @@
+ENV.each {|k,v| puts "#{k}: #{v}"}
+
 require 'xcodeproj'
-project = Xcodeproj::Project.open(path_to_project)
+project = Xcodeproj::Project.open("EmbeddedJvm.xcodeproj")
 main_target = project.targets.first
 phase = main_target.new_shell_script_build_phase("Link JVM files to app")
 phase.shell_script = "mkdir Plugins ; mkdir Plugins/Java ; do sth with ${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/your.file"
